7540f281feff077ff8ab47453565f9f0dff91d34
Israel Shaulov <israel236541@gmail.com>
2024-04-19 22:14:45 +0300

    complete post page functionality

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -133,7 +133,7 @@ const UpdatePost = () => {
               <option value='javascript'>JavaScript</option>
               <option value='reactjs'>React.js</option>
               <option value='nextjs'>Next.js</option>
-              <option value='angular'>Angular</option>
+              <option value='typescript'>TypeScript</option>
             </Select>
           </div>
           <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
